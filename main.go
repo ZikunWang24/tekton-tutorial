e373199af3d8005c31c90b79ea71bd8a49f30d95
Zikun Wang <35442448+ZikunWang24@users.noreply.github.com>
2021-07-01 14:26:13 +0800

    Update main.go
    sss

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "abd ndsfdsfsssssdfnnnnnnnnndc")
+	fmt.Fprintf(w, "abd ndsfdsfsxxxxssssdfnnnnnnnnndc")
 }
